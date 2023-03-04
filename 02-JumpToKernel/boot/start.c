9d8f72aaa53466dd3067c172daf95cf024a972d2
gahow <89721635@qq.com>
2023-03-04 11:55:29 +0800

    fix chapter 02

--- 02-JumpToKernel/boot/start.c
+++ 02-JumpToKernel/boot/start.c
@@ -74,11 +74,6 @@ void start() {
     // disable paging for now
     w_satp(0);
 
-    // delegate all interrupts and exceptions to supervisor mode.
-    w_medeleg(0xffff);
-    w_mideleg(0xffff);
-    w_sie(r_sie() | SIE_SEIE | SIE_STIE | SIE_SSIE);
-
     // configure Physical Memory Protection to give supervisor mode
     // access to all of physical memory.
     w_pmpcfg0(0xf);
